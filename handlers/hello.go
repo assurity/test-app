798be51425a10f40b92a60de28afd100a70ed088
assurity-ben <ben.craig@assurity.co.nz>
2022-10-04 15:32:24 +1300

    Updated header again

--- handlers/hello.go
+++ handlers/hello.go
@@ -17,7 +17,7 @@ func (p *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	styledTemplate.Execute(w, Body{Body: fmt.Sprintf(`
 <div class="hello">
-	Ben's App
+	Ben's Application
 </div>
 
 <div class="my-index">My Index Is</div>
